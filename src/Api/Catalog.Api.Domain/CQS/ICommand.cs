91769040678e9e8ae34182df5af4ed74adce7342
Maxime Poulain <maxime.poulain@outlook.be>
2022-03-20 20:41:13 +0100

    Domain: add base interfaces for commands and queries by using mediatR library

---
+++ src/Api/Catalog.Api.Domain/CQS/ICommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace Catalog.Api.Domain.CQS;
+
+public interface ICommand
+{
+
+}
+
+public interface ICommand<out TResponse> : ICommand, IRequest<TResponse>
+{
+
+}
+
+public interface ICommandHandler<in TCommand, TResponse> : IRequestHandler<TCommand, TResponse> where TCommand : ICommand<TResponse>
+{
+
+}
---
+++ src/Api/Catalog.Api.Domain/CQS/IQuery.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace Catalog.Api.Domain.CQS;
+
+public interface IQuery
+{
+
+}
+
+public interface IQuery<out TResponse> : IQuery, IRequest<TResponse>
+{
+
+}
+
+public interface IQueryHandler<in TQuery, TResponse> : IRequestHandler<TQuery, TResponse> where TQuery : IQuery<TResponse>
+{
+
+}
