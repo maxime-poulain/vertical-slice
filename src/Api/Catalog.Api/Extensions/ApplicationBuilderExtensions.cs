44fe5bd75fefc375e63b2bcf4e70070ac1be3da5
Maxime Poulain <maxime.poulain@outlook.be>
2022-03-20 19:11:40 +0100

    Add solution and projects for the API
    This commit introduces the default structure of our solution without any actual implementation.

---
+++ src/Api/Catalog.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Catalog.Api.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static IApplicationBuilder UseSwagger(this IApplicationBuilder app, IConfiguration configuration)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.InjectStylesheet("/swagger-ui/custom.css");
+        });
+
+        return app;
+    }
+}
---
+++ src/Api/Catalog.Api/Program.cs
@@ -0,0 +1,27 @@
+using Catalog.Api.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => options.EnableAnnotations());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsProduction())
+{
+    app.UseSwagger(builder.Configuration);
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
