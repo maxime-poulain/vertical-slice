48c01058b8c2ca638468bfdac8da6c2a99e5f0fc
Maxime Poulain <maxime.poulain@outlook.be>
2022-03-22 20:13:48 +0100

    API: add retrieval of every assignments

---
+++ src/Api/Catalog.Api.Application/Features/Assignments/GetAllAssignments/GetAllAssignmentsQuery.cs
@@ -0,0 +1,30 @@
+﻿using Catalog.Api.Domain.CQS;
+using Catalog.Api.EfCore.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Api.Application.Features.Assignments.GetAllAssignments;
+
+public class GetAllAssignmentsQuery : IQuery<IEnumerable<AssignmentDto>>
+{
+}
+
+public class GetAllAssignmentsQueryHandler : IQueryHandler<GetAllAssignmentsQuery, IEnumerable<AssignmentDto>>
+{
+    private readonly CatalogContext _catalogContext;
+
+    public GetAllAssignmentsQueryHandler(CatalogContext catalogContext)
+    {
+        _catalogContext = catalogContext;
+    }
+
+    public async Task<IEnumerable<AssignmentDto>> Handle(GetAllAssignmentsQuery request, CancellationToken cancellationToken)
+    {
+        return await _catalogContext.TrainingAssignment
+            .Select(assignment => new AssignmentDto()
+            {
+                TrainerId  = assignment.TrainerId,
+                TrainingId = assignment.TrainingId
+            })
+            .ToListAsync(cancellationToken);
+    }
+}
---
+++ src/Api/Catalog.Api/Endpoints/Assignment/All/AllAssignmentsEndpoint.cs
@@ -0,0 +1,31 @@
+﻿using Ardalis.ApiEndpoints;
+using Catalog.Api.Application.Features.Assignments;
+using Catalog.Api.Application.Features.Assignments.GetAllAssignments;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Catalog.Api.Endpoints.Assignment.All;
+
+public class AssignmentGetAll : EndpointBaseAsync.WithoutRequest.WithActionResult<IEnumerable<AssignmentDto>>
+{
+    private readonly IMediator _mediator;
+
+    public AssignmentGetAll(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet("training/assignment", Name = "Training.Assignment.All")]
+    [SwaggerOperation(
+        Summary = "Gets all the assignments",
+        Description = "Gets all the assignments",
+        OperationId = "Training.Assignment.All",
+        Tags = new[] { "Assignment" })]
+    [ProducesResponseType(typeof(IEnumerable<AssignmentDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status500InternalServerError)]
+    public override async Task<ActionResult<IEnumerable<AssignmentDto>>> HandleAsync(CancellationToken cancellationToken = default)
+    {
+        return Ok(await _mediator.Send(new GetAllAssignmentsQuery(), cancellationToken));
+    }
+}
